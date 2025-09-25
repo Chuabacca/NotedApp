f5a21e8da2d046e0086099024a373ccfe1817463
Chuabacca <jchua.yf@gmail.com>
2025-09-24 17:04:02 -0700

    Initial Commit

---
+++ NotedApp/NotedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotedAppApp.swift
+//  NotedApp
+//
+//  Created by Jonathan Chua on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct NotedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NotedAppTests/NotedAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NotedAppTests.swift
+//  NotedAppTests
+//
+//  Created by Jonathan Chua on 9/24/25.
+//
+
+import Testing
+@testable import NotedApp
+
+struct NotedAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
